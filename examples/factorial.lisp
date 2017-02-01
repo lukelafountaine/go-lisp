4a87df1170818a416d876156ed8e2c93b9336d7e
Luke LaFountaine <luke.lafountaine537@myci.csuci.edu>
2017-01-31 20:35:13 -0500

    added some examples

---
+++ examples/factorial.lisp
@@ -0,0 +1,2 @@
+(define fact (lambda (x) (if (== x 0) 1 (* x (fact (- x 1))))))
+(&& (== (fact 5) 120) (== (fact 10) 3628800))
---
+++ examples/gcd.lisp
@@ -0,0 +1,2 @@
+(define gcd (lambda (a b) (if (|| (< a 0) (< b 0)) ((gcd (abs a) (abs b))) (if (< a b) (gcd b a) (if (== b 0) a (gcd b (% a b)))))))
+(&& (== (gcd 13 13) 13) (== (gcd 37 600) 1) (== (gcd 20 100) 20) (== (gcd 624129 2061517) 18913))
