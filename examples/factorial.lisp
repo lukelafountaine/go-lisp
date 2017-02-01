cb4fd30fe39ccb0bd08c12350a390b09fdfd3b0a
Luke LaFountaine <luke.lafountaine537@myci.csuci.edu>
2017-01-31 22:31:01 -0500

    reformatted examples for easier reading

--- examples/factorial.lisp
+++ examples/factorial.lisp
@@ -1,2 +1,8 @@
-(define fact (lambda (x) (if (== x 0) 1 (* x (fact (- x 1))))))
-(&& (== (fact 5) 120) (== (fact 10) 3628800))
+(define fact (lambda (x)
+    (if (== x 0)
+        1
+        (* x (fact (- x 1))))))
+
+(&&
+    (== (fact 5) 120)
+    (== (fact 10) 3628800))
